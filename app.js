5c796105af6c22aa5fe739d7ec4c1272161b921a
artsnow12 <168270648+artsnow12@users.noreply.github.com>
2025-05-27 17:18:52 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const tooltipTriggerList = document.querySelectorAll('[title="Tooltip on right"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+"use strict";
 
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible hidden" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-appendAlert('Nice, you triggered this alert message!', 'success');
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    document.querySelector('[role="alert"]').classList.toggle('hidden');
-})
-}
+var myDateOfBirth = '28.01.1980';
+var formattedDate = moment(myDateOfBirth, 'DD.MM.YYYY').format('MMMM Do YYYY');
+var p = document.querySelector('.myB-day');
+p.textContent = "I was born on ".concat(formattedDate, ".");
+var inputUserBday = document.querySelector('#userB-day');
+inputUserBday.addEventListener('input', function (event) {
+  var message = document.querySelector('[role="alert"]');
+  if (/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$/.test(event.target.value)) {
+    var userFormattedDate = moment(event.target.value, 'DD.MM.YYYY').format('MMMM Do YYYY');
+    message.className = '';
+    message.classList.add("alert", "alert-success");
+    message.textContent = "You were born on ".concat(userFormattedDate);
+  } else {
+    message.classList.add("alert", "alert-danger");
+    message.textContent = 'The date entered is incorrect';
+  }
+});
